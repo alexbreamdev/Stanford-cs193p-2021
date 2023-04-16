83af99d535a4e2254d33ccc696668d36d1cb3877
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-04-16 18:07:55 +0300

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Oleksii Leshchenko on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
